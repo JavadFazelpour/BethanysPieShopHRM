aec2048243b9a5dba2e2623b69dd0d58370b89eb
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-04-29 15:31:42 +0330

    Add Bethany Shop App User Menu.

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,15 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using BethanysPieShopHRM.HR;
 
-IEmployee bethany = new StoreManager("Bethany", "Smith", "bethanysmith@pomokelhause.de", new DateTime(1979, 1, 11), 27);
+List<IEmployee> employees = new List<IEmployee>();
 
-Console.WriteLine("How many hours do we need register for Bethany?");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("*********************************");
+Console.WriteLine("*Bethany's Pie Shop Employee App*");
+Console.WriteLine("*********************************");
+Console.ForegroundColor = ConsoleColor.White;
 
-int numberOfHours = 0;
-string input = Console.ReadLine();
-numberOfHours = int.Parse(input);
+string userSelection;
+Console.ForegroundColor = ConsoleColor.Blue;
 
-bethany.DisplayEmployeeDetails();
-bethany.PerformWork();
-bethany.PerformWork(numberOfHours);
+//Utilities.CheckForExistingEmployeeFile();
+
+do
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"Loaded {employees.Count} employee(s)\n\n");
+
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("********************");
+    Console.WriteLine("* Select an Action *");
+    Console.WriteLine("********************");
+
+    Console.WriteLine("1: Register an employee");
+    Console.WriteLine("2: View all employees");
+    Console.WriteLine("3: Save Data");
+    Console.WriteLine("4: Load Data");
+    Console.WriteLine("9: Quit Application");
+
+} while (true);
